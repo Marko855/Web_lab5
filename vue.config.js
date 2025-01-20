0297048d2a9e98bed176e2d0deb5d19c67014e7b
Marko Dananic <markodananic@MacBook-Pro-od-Marko.local>
2025-01-20 21:43:56 +0100

    initial commit

--- vue.config.js
+++ vue.config.js
@@ -6,7 +6,7 @@ module.exports = defineConfig({
   configureWebpack: {
     plugins: [
       new webpack.DefinePlugin({
-        '__VUE_PROD_HYDRATION_MISMATCH_DETAILS__': JSON.stringify(false), // Set to boolean false
+        '__VUE_PROD_HYDRATION_MISMATCH_DETAILS__': JSON.stringify(false), 
         '__VUE_OPTIONS_API__': JSON.stringify(true),
         '__VUE_PROD_DEVTOOLS__': JSON.stringify(false),
       }),
